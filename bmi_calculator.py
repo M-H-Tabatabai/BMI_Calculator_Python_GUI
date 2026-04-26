a09becadff0358dd3c77fb1005ae40620b5f3e79
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-04-26 20:33:14 +0330

    feat: implement user-input function

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -2,7 +2,10 @@
 
 
 #get user inputs
-
+def user_input():
+    weight = input("please enter your weight (kg) : ")
+    height = input("please enter your height : (m.cm)")
+    return weight, height
 
 #calculate bmi
 
