545e2cbce6020d3bb89a8a3c2e9ebdb4f3b7c566
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-04-26 20:40:19 +0330

    feat: implement show results func

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -13,6 +13,17 @@ def calculate_bmi(weight, height):
     return bmi
 
 #show results
+def show_result(bmi):
+    if bmi < 18.5:
+        return "under weight"
+    elif bmi < 25:
+        return "normal weight"
+    elif bmi < 30:
+        return "over wight"
+    elif bmi < 35: 
+        return "obese"
+    else:
+        return "extremely obese"
 
 
 #main
