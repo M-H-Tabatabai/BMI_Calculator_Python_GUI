085a6e9b045105238eb07dd146bab80fd83d5419
Mohammad Hossein <m.h.tabatabai23@gmail.com>
2026-05-02 00:28:31 +0330

    feat(main) : Display output graphically

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,10 +1,11 @@
 #imports and define global variable
-
+import tkinter as tk 
+from tkinter import ttk 
 
 #get user inputs
 def user_input():
     weight = float(input("please enter your weight (kg) : "))
-    height = float(input("please enter your height : (m.cm)"))
+    height = float(input("please enter your height (m.cm) : "))
     return weight, height
 
 #calculate bmi
@@ -28,11 +29,20 @@ def show_result(bmi):
 
 #main
 def main():
+   
    weight, height =  user_input()
    bmi = calculate_bmi(weight, height)
    result = show_result(bmi)
-   print(bmi)
-   print(result)
+   bmi_txt, result_txt = bmi, result
+
+   root = tk.Tk()
+   root.title("Results") 
+   root.geometry("300x300")
+   label1 = ttk.Label(root, text=bmi_txt, font=("Arial", 14), anchor="center")  
+   label2 = ttk.Label(root, text=result_txt, font=("Arial", 14), anchor="center")
+   label1.grid(row=0, column=0, padx=40, pady=5)
+   label2.grid(row=1, column=0, padx=40, pady=5)
+   root.mainloop()
     
 if __name__ == "__main__":
     main()
